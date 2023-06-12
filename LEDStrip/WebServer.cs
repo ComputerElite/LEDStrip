9d1dc45d86c41ae3f0f654af10c526656f2ea31a
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2023-06-12 19:27:19 +0200

    remove unneeded files

--- LEDStrip/WebServer.cs
+++ LEDStrip/WebServer.cs
@@ -15,8 +15,6 @@ public class WebServer
         server.DefaultCacheValidityInSeconds = 0;
         server.AddRouteFile("/", html + "index.html", false, true, false);
         server.AddRouteFile("/style.css", html + "style.css", false, true, false);
-        server.AddRouteFile("/color-picker.css", html + "color-picker.css", false, true, false);
-        server.AddRouteFile("/color-picker.js", html + "color-picker.js", false, true, false);
         server.AddRoute("GET", "/api/animations", request =>
         {
             request.SendString(JsonSerializer.Serialize(Animation.GetAnimations()), "application/json");
